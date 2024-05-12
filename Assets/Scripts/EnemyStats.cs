d8ab8cf180b4a681512841028de840b493a852f4
Onur Çelik <celikonur988@gmail.com>
2024-05-12 20:28:06 +0300

    SO data classes created both enemy and playerUnit

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EntityStat", menuName = "ScriptableObjets/EnemyStats")]
+public class EnemyStats : EntityStats
+{
+    
+    
+}
---
+++ Assets/Scripts/PlayerUnitsStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EntityStat", menuName = "ScriptableObjets/PlayerUnitStats")]
+public class PlayerUnitsStats : EntityStats
+{
+    
+    
+}
