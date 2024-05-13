3b8488d640519740e1dba9897d3c982fa7865df7
Onur Çelik <celikonur988@gmail.com>
2024-05-13 23:49:55 +0300

    New Persona entity system in progress v2

--- Assets/Scripts/New Folder/Persona.cs
+++ Assets/Scripts/New Folder/Persona.cs
@@ -3,6 +3,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Character
+{
+    
+}
+
+public class CharacterAbilities : Character, ICharacterAbilities
+{
+    public void AbilityAction()
+    {
+        Debug.Log("This persona has " + Ability + " ability");
+    }
+
+    public Abilities Ability => Abilities.Dark;
+}
+
+public interface ICharacterAbilities : IAbility
+{
+    
+}
+
+public interface IAbility
+{
+    void AbilityAction();
+    Abilities Ability { get; }
+}
+
 public class Persona : MonoBehaviour
 {
     public int Health;
@@ -16,20 +42,45 @@ public class Persona : MonoBehaviour
     private List<IPersonaAttack> allPersona = new List<IPersonaAttack>();
 
     public List<PersonaAbilities> PersonaAbilitiesList = new List<PersonaAbilities>();
-    
-    
+
+    private List<IPersonaDefence> personaDefence = new List<IPersonaDefence>();
+    private void Awake()
+    {
+        personaDefence.Add(_slashPersonaStat);
+        personaDefence.Add(_strikePersonaStat);
+        personaDefence.Add(_piercePersonaStat);
+        
+        
+        _slashPersonaAbility.AbilityAction();
+
+        if (_characterAbilities.Ability == _slashPersonaAbility.Ability)
+        {
+            Debug.Log("Wassap Dog1");
+        }
+        else
+        {
+            Debug.Log("Wassap Dog2");
+        }
+    }
+
     private void Update()
     {
         Checkstat();
     }
 
-    private StrikeStat _strikeStat = new StrikeStat(true, true, false, false);
+    private StrikePersonaStat _strikePersonaStat = new StrikePersonaStat(true, true, false, false);
+    private SlashPersonaStat _slashPersonaStat = new SlashPersonaStat(true, true, true, true);
+    private PiercePersonaStat _piercePersonaStat = new PiercePersonaStat(true, true, true, true);
 
+    private SlashPersonaAbility _slashPersonaAbility = new SlashPersonaAbility();
+    private CharacterAbilities _characterAbilities = new CharacterAbilities();
+    
     private void Checkstat()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!Input.GetKeyDown(KeyCode.Space)) return;
+        foreach (var defence in personaDefence)
         {
-            _strikeStat.Defence();
+            defence.Defence();
         }
     }
 }
@@ -53,7 +104,13 @@ public interface IPersonaDefence
 {
     void Defence();
 }
-public class Stats
+
+public interface IPersonaAbilities : IAbility
+{
+    
+}
+
+public class PersonaStats
 {
     public bool Weakness;
     public bool Resistance;
@@ -61,7 +118,7 @@ public class Stats
     public bool Reflect;
 
 
-    public Stats(bool weakness, bool resistance, bool normal, bool reflect)
+    protected PersonaStats(bool weakness, bool resistance, bool normal, bool reflect)
     {
         Weakness = weakness;
         Resistance = resistance;
@@ -75,43 +132,86 @@ public class Stats
      */
 }
 
-public class StrikeStat : Stats, IPersonaDefence
+public class SlashPersonaStat : PersonaStats, IPersonaDefence
 {
-    public StrikeStat(bool weakness, bool resistance, bool normal, bool reflect) : base(weakness, resistance, normal, reflect)
+    public SlashPersonaStat(bool weakness, bool resistance, bool normal, bool reflect) : base(weakness, resistance, normal, reflect)
     {
     }
 
     public void Defence()
     {
-        if (Resistance)
-        {
-            Debug.Log("This Persona Has resistance to Strike Attacks");
-        }
-        if (Weakness)
-        {
-            Debug.Log("This Persona Has Weakness to Strike Attacks");
-        }
-        if (Normal)
-        {
-            Debug.Log("This Persona has no effect to Strike Attacks");
-        }
-        if (Reflect)
-        {
-            Debug.Log("This Persona Has Reflect to Strike Attacks");
-        }
+        if (Resistance) Debug.Log("This Persona Has resistance to Slash Attacks");
+        if (Weakness) Debug.Log("This Persona Has Weakness to Slash Attacks");
+        if (Normal) Debug.Log("This Persona has no effect to Slash Attacks");
+        if (Reflect) Debug.Log("This Persona Has Reflect to Slash Attacks");
+    }
+}
+public class PiercePersonaStat : PersonaStats, IPersonaDefence
+{
+    public PiercePersonaStat(bool weakness, bool resistance, bool normal, bool reflect) : base(weakness, resistance, normal, reflect)
+    {
+    }
+
+    public void Defence()
+    {
+        if (Resistance) Debug.Log("This Persona Has resistance to Pierce Attacks");
+        if (Weakness) Debug.Log("This Persona Has Weakness to Pierce Attacks");
+        if (Normal) Debug.Log("This Persona has no effect to Pierce Attacks");
+        if (Reflect) Debug.Log("This Persona Has Reflect to Pierce Attacks");
     }
 }
 
-public class PersonaStats
+
+public class StrikePersonaStat : PersonaStats, IPersonaDefence
 {
-    
+    public StrikePersonaStat(bool weakness, bool resistance, bool normal, bool reflect) : base(weakness, resistance, normal, reflect)
+    {
+    }
+
+    public void Defence()
+    {
+        if (Resistance) Debug.Log("This Persona Has resistance to Strike Attacks");
+        if (Weakness) Debug.Log("This Persona Has Weakness to Strike Attacks");
+        if (Normal) Debug.Log("This Persona has no effect to Strike Attacks");
+        if (Reflect) Debug.Log("This Persona Has Reflect to Strike Attacks");
+    }
 }
 
+public class SlashPersonaAbility : PersonaAbilities, IPersonaAbilities
+{
+    public void AbilityAction()
+    {
+        Debug.Log("This persona has " + Ability + " ability");
+    }
+
+    public Abilities Ability { get; }
+}
+
+public enum Abilities
+{
+    Slash,
+    Strike,
+    Pierce,
+    Fire,
+    Ice,
+    Electricity,
+    Wind,
+    Light,
+    Dark
+}
 public class PersonaAbilities
 {
+    protected Abilities _ability;
     
-    
-    
+    protected bool _slash;
+    protected bool _strike;
+    protected bool _pierce;
+    protected bool _fire;
+    protected bool _ice;
+    protected bool _electricity;
+    protected bool _wind;
+    protected bool _light;
+    protected bool _dark;
     
 }
 
