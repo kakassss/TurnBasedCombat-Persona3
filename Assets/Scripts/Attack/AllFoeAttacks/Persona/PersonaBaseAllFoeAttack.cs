d06196a9eeb73da2b028a6161d3df74c1ae78ef9
Onur Çelik <celikonur988@gmail.com>
2024-05-22 01:41:25 +0300

    all foe attack scripts

---
+++ Assets/Scripts/Attack/AllFoeAttacks/Persona/PersonaBaseAllFoeAttack.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Enums;
+using Interfaces;
+using Interfaces.Stats;
+using SignalBus;
+using UnityEngine;
+
+namespace Attack.AllFoeAttacks
+{
+    public class PersonaBaseAllFoeAttack : EntityBaseAttackData, IPersonaAttack
+    {
+        public Stat Stat => _stat;
+        public AttackTypes AttackTypes => _attackTypes;
+        public string AttackName => _attackName;
+        public int AttackDamageToItself => _attackDamageToItself;
+    
+        public void AttackAction(IMove activeEntity, List<IMove> allDeactiveEntities)
+        {
+            activeEntity.entity.TakeDamageUsingAttack(_attackDamageToItself);
+            foreach (var deactiveEntity in allDeactiveEntities)
+            {
+                deactiveEntity.entity.TakeDamage((activeEntity.entity.entityBaseSo.BaseAttackValue + _attackDamageToEnemy) * (int)_attackTypes);
+            }
+            
+            Debug.Log("Persona " + Stat + " Attack! " + "Total Damage: " + (activeEntity.entity.entityBaseSo.BaseAttackValue + _attackDamageToEnemy) * (int)_attackTypes);
+            
+            activeEntity.MoveAction();
+            EventBus<OnHealthChanged>.Fire(new OnHealthChanged());
+            EventBus<OnTakeDamage>.Fire(new OnTakeDamage
+            {
+                Stat =  _stat
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/Attack/AllFoeAttacks/Persona/PiercePersonaAllFoeAttack.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Attack.AllFoeAttacks
+{
+    [CreateAssetMenu(fileName = "PiercePersonaAllFoeAttack", menuName = "ScriptableObjets/PersonaAllFoeAttack/PiercePersonaAllFoeAttack")]
+    public class PiercePersonaAllFoeAttack : PersonaBaseAllFoeAttack
+    {
+   
+    
+    }
+}
