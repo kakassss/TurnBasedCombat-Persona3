7b345298b3fabf1b966448b5381a403bcada0c36
Onur Çelik <celikonur988@gmail.com>
2024-05-22 01:39:50 +0300

    Persona All foe attack mechanic implemented to system

---
+++ Assets/Scripts/Attack/AllFoeAttacks/SlashPersonaAllFoeAttack.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Attack.AllFoeAttacks
+{
+    [CreateAssetMenu(fileName = "SlashPersonaAllFoeAttack", menuName = "ScriptableObjets/PersonaAllFoeAttack/SlashPersonaAllFoeAttack")]
+    public class SlashPersonaAllFoeAttack : PersonaBaseAllFoeAttack
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Attack/AllFoeAttacks/StrikePersonaAllFoeAttack.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Attack.AllFoeAttacks
+{
+    [CreateAssetMenu(fileName = "StrikePersonaAllFoeAttack", menuName = "ScriptableObjets/PersonaAllFoeAttack/StrikePersonaAllFoeAttack")]
+    public class StrikePersonaAllFoeAttack : PersonaBaseAllFoeAttack
+    {
+        
+    }
+}
