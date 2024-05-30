01ff1ab1f4e136b4a9692aa2948c803e1e8b42df
Onur Çelik <celikonur988@gmail.com>
2024-05-31 00:41:21 +0300

    Feature: general close button listener added

---
+++ Assets/Scripts/Battle/UI/Common/CloseButtonListener.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Battle.UI.Common
+{
+    public class CloseButtonListener : MonoBehaviour
+    { 
+        private Button _button;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(Close);
+        }
+
+
+        private void Close()
+        {
+            this.gameObject.SetActive(false);
+        }
+    
+    }
+}
